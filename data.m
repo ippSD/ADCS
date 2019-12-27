b56a0c574d0dd4f9a96a40a76ec7f6bdf3cfc411
Imanol Sardón <sardondelgadoimanol@gmail.com>
2019-12-27 12:38:28 +0100

    Blanca's blocks reconnected. Overall Model update. VRML Sun position reconnected, now plotting well.

--- data.m
+++ data.m
@@ -13,10 +13,12 @@ inertia = [0.035, 0., 0.; 0., 0.02, 0.; 0., 0., 0.05];
 
 a = str2num('BSPI');
 b = str2num('BMRS');
+month = mod(a, 10); % mes
+julian_date = juliandate([2018,month,1,12,0,0]);
+ut = 12;
 
 %% Orbit Parameters
 h = 400. + b;  % km
-month = mod(a, 10); % mes
 inc = a; % deg
 raan = b; % deg
 nu_0 = 0e0;  % deg
@@ -61,6 +63,52 @@ q0(1) = (c_bi(2,3) - c_bi(3,2)) / (2e0 * s_p) * sin(phi / 2e0);
 q0(2) = (c_bi(3,1) - c_bi(1,3)) / (2e0 * s_p) * sin(phi / 2e0);
 q0(3) = (c_bi(1,2) - c_bi(2,1)) / (2e0 * s_p) * sin(phi / 2e0);
 q0(4) = cos(phi/2e0);
+
+%% Bianca's ephem data
+
+%clear, clc
+%%%%5
+i=inc;
+%raan=raan;
+rt=RP;
+%r=r;
+period=period_sc;
+ta=0e0;
+
+%%%%
+
+d=julian_date;
+%ut=ut;
+T=(d+ut/24-2451545)/36525;
+
+L=mod(280.46+36000.77*T,360);
+G=mod(357.528+35999.05*T,360);
+lambda=L+1.915*sind(G)+0.02*sind(2*G);
+epsilon=23.4393-0.013*T;
+
+E=-1.915*sind(G)-0.02*sind(2*G)+2.4466*sind(2*lambda)-0.053*sind(4*lambda);
+GHA=15*ut-180+E;
+delta=asind(sind(epsilon)*sind(lambda));
+alfa=69;
+g2=alfa+GHA;
+SD=0.267/(1-0.017*cosd(G));
+g1=[cosd(delta) 0 -sind(delta); 0 1 0;sind(delta) 0 cosd(delta)];
+g2=[cosd(g2) -sind(g2) 0;sind(g2) cosd(g2) 0; 0 0 1];
+v=g1*g2*[1; 0 ;0];
+%%%%%%%%%%%%%%
+R=([cosd(raan) -sind(raan) 0; sind(raan) cosd(raan) 0; 0 0 1]*[1 0 0; 0 cosd(i) -sind(i);0 sind(i) cosd(i)]);
+C_opf=R';
+C_opr=[ cosd(ta) sind(ta) 0; -sind(ta) cosd(ta) 0; 0 0 1];
+C_lv=[0 1 0;0 0 -1;-1 0 0];
+r_lv=[0;0;r];
+ne_body=C_opf*C_opr*C_lv*r_lv;
+ne_body=ne_body/norm(ne_body);
+ns_body=v+ne_body;
+%%%%%%%%%%%%%%%%%%%%%%
+aa=asind(rt/r);
+es=180+(90-aa);
+ee=360-(90-aa);
+eclipse=[es, ee];
 
 %% Simulation Parameters
 
